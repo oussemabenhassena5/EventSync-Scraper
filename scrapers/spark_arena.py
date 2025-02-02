66e04025fc58bb1933e5713195d47da091d85384
oussemabenhassena5 (github) <oussemabenhassena5@gmail.com>
2025-02-02 17:40:53 +0100

    fix description scraping

--- scrapers/spark_arena.py
+++ scrapers/spark_arena.py
@@ -61,8 +61,9 @@ def scrape_spark_arena():
                         detail_page.goto(full_url)
                         detail_page.wait_for_load_state("networkidle")
                         time.sleep(2)
-                        detail_page.query_selector("div.rich-text-module")
-                        description = description_element.inner_text().strip()
+                        detail_page.query_selector(
+                            "div.rich-text-module"
+                        ).inner_text().strip()
                         detail_page.close()
                     except Exception as e:
                         print(
